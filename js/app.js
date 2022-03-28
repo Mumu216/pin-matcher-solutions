3dcf70dedbddbc826017372c1cd751e36f47e012
Mumu216 <farzanamumu752@gmail.com>
2022-03-29 00:10:39 +0600

    update task

---
+++ js/app.js
@@ -0,0 +1,56 @@
+function getPin(){
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+     
+    if(pinString.length == 4){
+        return pin;
+    }
+    else{
+        console.log('got 3 digit and calling again', pin);
+        return getPin();
+    }
+
+    
+}
+
+function generatePin(){
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
+}
+
+document.getElementById('key-pad').addEventListener('click',function(event){
+     const number = event.target.innerText;
+     const calcInput = document.getElementById('typed-numbers');
+     if(isNaN(number)){
+         if(number == 'C'){
+             calcInput.value = '';
+         }
+         console.log(number);
+     }
+     else{
+
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+     }
+    
+});
+
+function verifyPin(){
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const notifyMessage = document.getElementById('notify-success');
+    const notifyError = document.getElementById('notify-fail');
+
+
+    if(pin == typedNumbers){
+        notifyMessage.style.display = 'block';
+        notifyError.style.display = 'none';
+
+    }
+    else{
+
+        notifyMessage.style.display = 'none';
+        notifyError.style.display = 'block';
+    }
+}
